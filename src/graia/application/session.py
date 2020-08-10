d4eac835ba28d4dff03b8079cc670f7fad2785b8
GreyElaina <31543961+GreyElaina@users.noreply.github.com>
2020-08-10 22:12:49 +0800

    完善了 Session(会话对象) 的内嵌文档

--- src/graia/application/session.py
+++ src/graia/application/session.py
@@ -3,7 +3,16 @@ from typing import Tuple
 import yarl
 
 class Session(BaseModel):
-    """用于描述与上游接口会话, 并存储会话状态的实体类."""
+    """用于描述与上游接口会话, 并存储会话状态的实体类.
+    
+    Attributes:
+        host (AnyHttpUrl): `mirai-api-http` 服务所在的根接口地址
+        authKey (str): 在 `mirai-api-http` 配置流程中定义, 需为相同的值以通过安全验证.
+        account (int): 应用所使用账号的整数 ID.
+        websocket (bool): 是否使用 Websocket 方式获取事件, 若为 `False` 则使用 HTTP 短轮询方式获取事件, 性能较低.
+        sessionKey (str, optional): 会话中用于进行操作的唯一认证凭证, 需要经过 `activeSession` 后才可用.
+        current_version (Tuple[int, int, int], optional): 上游服务的版本, 暂时没有自动获取.
+    """
 
     host: AnyHttpUrl
     authKey: str
@@ -14,6 +23,15 @@ class Session(BaseModel):
 
     @classmethod
     def fromUrl(cls, url: str) -> "Session":
+        """不再推荐的声明连接方式的方法, 用于兼容 v3 中的 url 声明连接信息的方法.
+
+        Args:
+            url (str): `schema` 为 `mirai` 的链接, 具体请参考 python-mirai(v3) 的文档.
+        
+        Returns:
+            Session: 从给出的 url 生成的 `Session` 实例.
+        """
+
         yarl_url = yarl.URL(url)
         return cls(
             host=str(yarl.URL.build(
