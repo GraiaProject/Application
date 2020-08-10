9f3d9afd680b68b99f23f60202e2b4ccf2d8c662
GreyElaina <31543961+GreyElaina@users.noreply.github.com>
2020-08-10 22:02:36 +0800

    添加了除 application 本体模块之外的所有用户接口模块的简短介绍文档

--- src/graia/application/entities.py
+++ src/graia/application/entities.py
@@ -2,10 +2,12 @@ from enum import Enum
 from pydantic import BaseModel
 
 class UploadMethods(Enum):
+    """用于向 `uploadImage` 方法描述图片的上传类型"""
     Friend = "friend"
     Group = "group"
     Temp = "temp"
 
 class MiraiConfig(BaseModel):
+    """`/config` 接口的序列化实体类"""
     cacheSize: int
     enableWebsocket: bool
