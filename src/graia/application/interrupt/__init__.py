334284a1b8464459faec744f141bd34dfb4755ef
GreyElaina <31543961+GreyElaina@users.noreply.github.com>
2020-08-15 21:09:31 +0800

    add block propagation feature for interrupt

--- src/graia/application/interrupt/__init__.py
+++ src/graia/application/interrupt/__init__.py
@@ -6,7 +6,19 @@ from graia.broadcast.entities.event import BaseEvent
 from abc import ABCMeta, abstractmethod
 from graia.broadcast.utilles import run_always_await
 from graia.broadcast.entities.signatures import RemoveMe
+from graia.broadcast.exceptions import PropagationCancelled
 import asyncio
+
+class ActiveStats:
+    def __init__(self) -> None:
+        self.actived = False
+
+    def get(self) -> bool:
+        return self.actived
+    
+    def set(self) -> bool:
+        self.actived = True
+        return True
 
 class Interrupt(metaclass=ABCMeta):
     """内置中断(Interrupt)机制中, 中断项的抽象实现, 需要重写 `trigger` 方法, 该方法接受一个 event 参数,
@@ -14,6 +26,7 @@ class Interrupt(metaclass=ABCMeta):
     """
     direct: Type[BaseEvent]
     _return_value: Any = None
+    _block_propagation: bool = False
 
     def set_return_value(self, value: Any):
         self._return_value = value
@@ -44,13 +57,19 @@ class InterruptControl:
         return interrupt.get_return_value()
 
     def leader_listener_generator(self, event_lock: asyncio.Event, interrupt: Interrupt):
+        active_stat = ActiveStats()
         async def inside_listener(event: interrupt.direct):
+            if active_stat.get():
+                return RemoveMe()
             try:
                 result = await run_always_await(interrupt.trigger(event))
                 if result is not None:
+                    active_stat.set()
                     event_lock.set()
                     interrupt.set_return_value(result)
-                    return RemoveMe()
+                    if not interrupt._block_propagation:
+                        return RemoveMe()
+                    raise PropagationCancelled()
             except:
                 import traceback
                 traceback.print_exc()
