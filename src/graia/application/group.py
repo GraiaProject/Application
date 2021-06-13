1e93d332420f1da9021efbe9ac6c8389687027ed
kamaellee <74295299+kamaellee@users.noreply.github.com>
2021-06-14 02:35:51 +0800

    群文件组件增加
    新增 FileList 、FIleConfig 类分别对应 群文件列表、群文件详细信息 返回承载

--- src/graia/application/group.py
+++ src/graia/application/group.py
@@ -55,3 +55,26 @@ class MemberInfo(BaseModel):
 
     class Config:
         allow_mutation = True
+
+class FileList(BaseModel):
+    "描述群组文件的有关状态"
+
+    name: Optional[str] = None
+    id: Optional[str] = None
+    path: Optional[str] = None # 文件路径
+    isFile: Optional[bool] = None # 是否为文件
+
+class FileConfig(BaseModel):
+    """群组文件详细信息"""
+
+    name: Optional[str] = None
+    path: Optional[str] = None
+    id: Optional[str] = None
+    length: Optional[int] # 文件长度
+    downloadTimes: Optional[int] # 下载次数
+    uploaderId: Optional[int]  # 上传者QQ
+    uploadTime: datetime  # 上传时间
+    lastModifyTime: datetime  # 最后修改时间
+    downloadUrl: Optional[str] = None # 文件下载链接
+    sha1: Optional[str] = None # 文件sha1值
+    md5: Optional[str] = None # 文件md5值
